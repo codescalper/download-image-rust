a5340121e05e04665b1786044c4713a52c229ada
codescalper <msfunbook@gmail.com>
2023-10-31 22:54:10 +0530

    Fixed the corrupt file error

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ error_chain! {
 #[tokio::main]
 async fn main() -> Result<()> {
     let tmp_dir = Builder::new().prefix("example").tempdir()?;
-    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let target = "https://scontent-bom1-2.cdninstagram.com/v/t51.2885-19/299393232_1466816717167701_8169088999936650689_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-bom1-2.cdninstagram.com&_nc_cat=109&_nc_ohc=vr2WXBRWKjgAX-462Yq&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAdKOwYUJel0-QSK39DjtIFdKrFrwmOs3ZgkyIZ8Mqh8Q&oe=6545A6E0&_nc_sid=8b3546";
     let response = reqwest::get(target).await?;
     let download_directory = "C:\\Users\\mayank singh\\Downloads"; // Replace with your desired directory path
 
@@ -35,8 +35,10 @@ async fn main() -> Result<()> {
     };
 
     let response = reqwest::get(target).await?;
-    let content = response.text().await?;
-    copy(&mut content.as_bytes(), &mut dest)?;
+    let content = response.bytes().await?; // Changing this to .byutes() from .text() will download the file as binary
+                                           // fixed this error from stackoverflow https://stackoverflow.com/questions/63879853/why-is-a-png-image-downloaded-by-reqwest-corrupt
+    let mut cursor = std::io::Cursor::new(content);
+    std::io::copy(&mut cursor, &mut dest)?;
 
     Ok(())
 }
